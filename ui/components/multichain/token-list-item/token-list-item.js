3d0bbd5c98595c5488606b5f634cded1925c0511
Ali Zaryab Naeem <58973388+aleezaryabnaeem@users.noreply.github.com>
2024-05-24 12:12:46 +0500

    Border ICon Added

--- ui/components/multichain/token-list-item/token-list-item.js
+++ ui/components/multichain/token-list-item/token-list-item.js
@@ -188,6 +188,7 @@ export const TokenListItem = ({
         marginRight={3}
       >
         <AvatarToken
+          className={"borderRounded"}
           name={tokenSymbol}
           src={tokenImage}
           showHalo

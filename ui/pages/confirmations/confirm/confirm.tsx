0a704b09b2a39fc467415b6b2554b53fe2d838ce
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-05-28 16:38:01 +0100

    wip

--- ui/pages/confirmations/confirm/confirm.tsx
+++ ui/pages/confirmations/confirm/confirm.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-
 import { AlertActionHandlerProvider } from '../../../components/app/alert-system/contexts/alertActionHandler';
-import ScrollToBottom from '../components/confirm/scroll-to-bottom';
 import { Footer } from '../components/confirm/footer';
 import { Header } from '../components/confirm/header';
 import { Info } from '../components/confirm/info';
+import ScrollToBottom from '../components/confirm/scroll-to-bottom';
 ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
 import { MMISignatureMismatchBanner } from '../../../components/app/mmi-signature-mismatch-banner';
 ///: END:ONLY_INCLUDE_IF
+import { Page } from '../../../components/multichain/pages/page';
 import { Nav } from '../components/confirm/nav';
 import { Title } from '../components/confirm/title';
-import { Page } from '../../../components/multichain/pages/page';
 import setCurrentConfirmation from '../hooks/setCurrentConfirmation';
 import syncConfirmPath from '../hooks/syncConfirmPath';
 import { LedgerInfo } from '../components/confirm/ledger-info';
@@ -22,12 +21,16 @@ const Confirm = () => {
   syncConfirmPath();
   setConfirmationAlerts();
   const processAction = useConfirmationAlertActions();
+  const [showAdvancedDetails, setShowAdvancedDetails] = React.useState(false);
 
   return (
     <AlertActionHandlerProvider onProcessAction={processAction}>
       <Page className="confirm_wrapper">
         <Nav />
-        <Header />
+        <Header
+          showAdvancedDetails={showAdvancedDetails}
+          setShowAdvancedDetails={setShowAdvancedDetails}
+        />
         {
           ///: BEGIN:ONLY_INCLUDE_IF(build-mmi)
           <MMISignatureMismatchBanner />
@@ -36,7 +39,7 @@ const Confirm = () => {
         <ScrollToBottom>
           <LedgerInfo />
           <Title />
-          <Info />
+          <Info showAdvancedDetails={showAdvancedDetails} />
         </ScrollToBottom>
         <Footer />
       </Page>

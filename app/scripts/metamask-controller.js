5e993f75bd2a89f5dd1217167b924156438096ed
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-06-04 13:23:52 +0100

    wip

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -6104,8 +6104,6 @@ export default class MetamaskController extends EventEmitter {
   };
 
   resolvePendingApproval = async (id, value, options) => {
-    console.log('inside resolvePendingApproval', { id, value, options });
-
     try {
       await this.approvalController.accept(id, value, options);
     } catch (exp) {

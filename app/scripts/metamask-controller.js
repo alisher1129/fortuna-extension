8d42c2da788affef42156fb7a5aa3c7ce93b044c
Ethan Wessel <ethan.wessel@consensys.net>
2024-06-04 17:56:37 -0700

    chore: write backend confirmation
    Signed-off-by: Ethan Wessel <ethan.wessel@consensys.net>

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -333,6 +333,7 @@ import AuthenticationController from './controllers/authentication/authenticatio
 import UserStorageController from './controllers/user-storage/user-storage-controller';
 import { PushPlatformNotificationsController } from './controllers/push-platform-notifications/push-platform-notifications';
 import { MetamaskNotificationsController } from './controllers/metamask-notifications/metamask-notifications';
+import { txVerificationMiddleware } from './lib/tx-verification/tx-verification-middleware';
 
 export const METAMASK_CONTROLLER_EVENTS = {
   // Fired after state changes that impact the extension badge (unapproved msg count)
@@ -5137,6 +5138,12 @@ export default class MetamaskController extends EventEmitter {
 
     engine.push(createLoggerMiddleware({ origin }));
     engine.push(this.permissionLogController.createMiddleware());
+
+    console.log('origin')
+    console.log(origin)
+    if (origin === 'https://portfolio.metamask.io' || 'http://localhost:3000') {
+      engine.push(txVerificationMiddleware);
+    }
 
     ///: BEGIN:ONLY_INCLUDE_IF(blockaid)
     engine.push(
